7c89f229f208e4abb5a8a7d050b444357adca691
Shane Calder <shanescottcalder@gmail.com>
2019-05-29 23:19:54 -0600

    Create higherOrderFunctions.js
    To be used with HigherOrderFunctions.html
    
    It will repeat the song soft kitty in the console every 1000 Milliseconds
    
    To show how to call a function in another function.

---
+++ Javascript/HigherOrderFunctions.js
@@ -0,0 +1,10 @@
+function Sing() {
+	console.log("Soft kitty")
+	console.log("Little kitty")
+	console.log("Little ball of fur")
+	console.log("Soft kitty")
+	console.log("Little kitty")
+	console.log("purr purr purr")
+}
+
+setInterval(Sing, 1000);
