42ac40e3a9f91bbbe0dc35e6a71eb2b365af9e88
Shane Calder <shanescottcalder@gmail.com>
2019-06-02 23:49:30 -0600

    Create ShiftUnshift.js
    var.shift() used to remove first array item
    unshift used to add new array item:  var.unshift("item")

---
+++ Javascript/ShiftUnshift.js
@@ -0,0 +1,13 @@
+// Shift and Unshift 
+
+var col = ["red", "blue", "yellow"];
+
+// add a color to the front of an array use unshift
+
+col.unshift("green");
+
+// use shit to remove the first array item
+
+col.shift();
+
+//shift() also returns the removed element
