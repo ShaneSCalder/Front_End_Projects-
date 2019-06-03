75f6cdabd707932d85f60e503d6173be0cbf1b85
Shane Calder <shanescottcalder@gmail.com>
2019-06-02 23:35:26 -0600

    Create PushAndPop.js
    Example how to use push
    Example on how to use pop

---
+++ Javascript/PushAndPop.js
@@ -0,0 +1,15 @@
+// Push and pop
+
+var colours = ["red", "orange", "blue",]:
+
+// Use push to send item to the end of array
+
+colours.push("green");
+
+// Use pop to remove the last item
+
+colours.pop();
+// This will removed blue
+
+var col = colours.pop();
+// this will remove orange from the new var col
