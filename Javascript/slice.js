e8dbdcfcea1920a8df20c9d6ef7b60fa96441533
Shane Calder <shanescottcalder@gmail.com>
2019-06-03 00:09:35 -0600

    Create slice.js
    How to use slice() in an array

---
+++ Javascript/slice.js
@@ -0,0 +1,9 @@
+// Use slice to copy parts of an array 
+
+var nums = ["22", "33", "44", "55", "66", "77", "88"];
+
+// will copy 33, 44, 55 
+var someNums = nums.slice(1,4);
+
+//copy all
+var moreNums = nums.slice();
